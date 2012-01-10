ebd60896a87387e60c01ef6011cd713ed2824e00
Dan Mosedale <dmose@meer.net>
2012-01-09 16:14:10 -0800

    Replace the old action.js cloned from Mesh with one cloned from Script so that at least the constructor completes.

--- src/logic/action-list/resource/action.js
+++ src/logic/action-list/resource/action.js
@@ -4,20 +4,31 @@
 
 define( function ( require ) {
 
-    return function( engine, context ) {
-        
+    return function( engine ) {
+    
         var Action = engine.base.Resource({
             type: 'Action',
             cache: null
         },
         function( source ) {
-
+            
             source = source || {};
+            source.text = source.text || '';
+            source.parameters = source.parameters || [];
 
-            this._action = new context.Action( source );
+            var _script = new Function( source.parameters, source.text );
+            
+            Object.defineProperty( this, 'run', {
+                get: function() {
+                    return _script;
+                }
+            });
+            
         });
-
+        
         return Action;
+        
     };
-
+    
 });
+
